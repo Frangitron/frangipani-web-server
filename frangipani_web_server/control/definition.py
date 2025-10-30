b47bc7a3a5d74bc06f951470e531c4eba32ee134
MrFrangipane <contact@frangipane.me>
2025-10-30 20:44:32 +0100

    fix: Group + wip: begin orientation

--- frangipani_web_server/control/definition.py
+++ frangipani_web_server/control/definition.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 
 from frangipani_web_server.control.base import BaseWebControlDefinition
-from frangipani_web_server.control.type_enum import ControlTypeEnum
+from frangipani_web_server.control.enum_orientation import ControlOrientationEnum
+from frangipani_web_server.control.enum_type import ControlTypeEnum
 
 
 @dataclass_json
@@ -13,5 +14,6 @@ class WebControlDefinition(BaseWebControlDefinition):
     type: ControlTypeEnum
     value: float | bool
 
+    orientation: ControlOrientationEnum = ControlOrientationEnum.Horizontal
     max: float | None = None
     min: float | None = None
--- frangipani_web_server/control/group.py
+++ frangipani_web_server/control/group.py
@@ -1,14 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from dataclasses_json import dataclass_json
 
 from frangipani_web_server.control.base import BaseWebControlDefinition
 from frangipani_web_server.control.definition import WebControlDefinition
-from frangipani_web_server.control.type_enum import ControlTypeEnum
+from frangipani_web_server.control.enum_type import ControlTypeEnum
 
 
 @dataclass_json
 @dataclass
 class WebControlGroupDefinition(BaseWebControlDefinition):
     controls: list[WebControlDefinition]
-    type = ControlTypeEnum.Group
+    type: ControlTypeEnum = field(default=ControlTypeEnum.Group)
